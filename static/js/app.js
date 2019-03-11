27fdff19e13085b6bda6fccdede47d648e3e5240
JessieLiroff <jessicaliroff@gmail.com>
2019-03-11 15:39:39 -0700

    Update app.js

--- static/js/app.js
+++ static/js/app.js
@@ -3,31 +3,28 @@ var tableData = data;
 
 // Get a reference to the table body
 
-// Maybe clear out the existing data on the front end
-
 function generateTable(data){
 
 var tbody = d3.select("tbody");
 
+//clear out the existing data on the front end
 tbody.html("");
 
-// Console.log the weather data from data.js
-// console.log(tableData);
+// Console.log the UFO data from data.js
+console.log(tableData);
 
-// Step 1: Loop Through `data` and console.log each weather report object
+// Step 1: Loop Through `data` and console.log each UFO report object
 data.forEach(function(UFOreport){
-	// console.log(UFOreport);
-// Step 2:  Use d3 to append one table row `tr` for each weather report object
+	console.log(UFOreport);
+// Step 2:  Use d3 to append one table row `tr` for each UFO report object
 	var row = tbody.append("tr");
-// Don't worry about adding cells or text yet, just try appending the `tr` elements.
 
-// Step 3:  Use `Object.entries` to console.log each weather report value
+// Step 3:  Use `Object.entries` to console.log each UFO report value
 	Object.entries(UFOreport).forEach(function([key, value]){
-		// console.log(key,value);
-		// Step 4: Use d3 to append 1 cell per weather report value (weekday, date, high, low)
+		console.log(key,value);
+		// Step 4: Use d3 to append 1 cell per UFO report value (weekday, date, high, low)
 		var cell = row.append("td");
 		// Step 5: Use d3 to update each cell's text with
-		// weather report values (weekday, date, high, low)
 		cell.text(value);
 
 	});
@@ -35,8 +32,6 @@ data.forEach(function(UFOreport){
 
 generateTable(data);
 
-
-// some code that only executes when the input box gets changed 
 
 function handleClick(){
 
@@ -61,12 +56,6 @@ function handleClick(){
 
 // // filter the dataset 
 d3.selectAll('#filter-btn').on("click",handleClick);
-
-
-
-// some function has to regenerate the table using the value #datetime
-
-
 
 
 
